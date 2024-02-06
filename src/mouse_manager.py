d76ebaddb7657fbd455eb46c301f45d77e0e3ff0
JangSoyun <soyoon5328@gmail.com>
2024-02-06 21:29:29 +0900

    refact: rename method's name and edit console print

--- src/mouse_manager.py
+++ src/mouse_manager.py
@@ -6,12 +6,11 @@ class MousePoint(NamedTuple):
     x:int
     y:int
 
-def get_mouse_point()-> MousePoint:
-    print("[ Program ] Please move mouse to target point in 3 seconds...")
+def capture_mouse_position()-> MousePoint:
+    print("[ Mouse Manager ] Please move mouse to target point in 3 seconds...")
     time.sleep(3)
     x,y = input_device.position()
-    print("[ Program ]")
     return MousePoint(x,y)
     
-def move_mouse_point(target:MousePoint, time:float)->None:
+def move_mouse_to(target:MousePoint, time:float)->None:
     input_device.moveTo(target.x, target.y, time)
